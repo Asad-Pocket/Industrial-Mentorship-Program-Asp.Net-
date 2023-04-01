de87b41416ee7bf022f78e505b3289ba0b76bd6a
Asadul Islam <asadmail119@gmail.com>
2023-04-02 03:14:10 +0600

    statements and operation with text completed and example added

---
+++ Examples/statements and operation with text/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n, m, i, j;
+        char c = ' ';
+        string[] a;
+
+        string[] inputs = Console.ReadLine().Split();
+        n = int.Parse(inputs[0]);
+        m = int.Parse(inputs[1]);
+
+        for (i = 0; i < n; i++)
+        {
+            a = Console.ReadLine().Split();
+            //if first digit is " ";
+            if (a[0][0] == c)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            c = a[0][0];
+
+            for (j = 1; j < m; j++)
+                if (a[j][0] != c)
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+        }
+
+        Console.WriteLine("YES");
+    }
+}
